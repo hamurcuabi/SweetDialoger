8b09c8bd317e233c21b8160198d715ce024ea051
Emre Hamurcu <emrehamurcu_1995@hotmail.com>
2019-10-31 15:42:00 +0300

    added some methodes

--- sweetdialoglib/src/main/java/com/emrhmrc/sweetdialoglib/DialogCreater.java
+++ sweetdialoglib/src/main/java/com/emrhmrc/sweetdialoglib/DialogCreater.java
@@ -6,14 +6,6 @@ import android.graphics.Color;
 public class DialogCreater {
 
     private static String LOADING_COLOR = "#A5DC86";
-    private static String SURE = "Emin misiniz?";
-    private static String YES = "Evet";
-    private static String NO = "Hayır";
-    private static String INFO = "Bilgi";
-    private static String LOADING = "Yükleniyor";
-    private static String SUCCES = "İşlem Başarılı";
-    private static String FAIL = "İşlem Başarısız";
-    private static String OK = "Tamam";
 
     public static void basicDialog(Activity activity, String title) {
         new SweetAlertDialog(activity)
@@ -30,34 +22,36 @@ public class DialogCreater {
 
     public static void errorDialog(Activity activity, String content) {
         new SweetAlertDialog(activity, SweetAlertDialog.ERROR_TYPE)
-                .setTitleText(INFO)
+                .setTitleText(activity.getResources().getString(R.string.info))
                 .setContentText(content)
                 .show();
     }
-    public static void errorDialog(Activity activity,  String title,String content) {
+
+    public static void errorDialog(Activity activity, String title, String content) {
         new SweetAlertDialog(activity, SweetAlertDialog.ERROR_TYPE)
                 .setTitleText(title)
                 .setContentText(content)
                 .show();
     }
+
     public static void warningDialog(Activity activity, String content) {
         new SweetAlertDialog(activity, SweetAlertDialog.WARNING_TYPE)
-                .setTitleText(INFO)
+                .setTitleText(activity.getResources().getString(R.string.info))
                 .setContentText(content)
                 .show();
     }
 
     public static void succesDialog(Activity activity, String content) {
         new SweetAlertDialog(activity, SweetAlertDialog.SUCCESS_TYPE)
-                .setTitleText(INFO)
+                .setTitleText(activity.getResources().getString(R.string.info))
                 .setContentText(content)
                 .show();
     }
 
     public static void succesDialog(Activity activity) {
         new SweetAlertDialog(activity, SweetAlertDialog.SUCCESS_TYPE)
-                .setTitleText(INFO)
-                .setContentText(SUCCES)
+                .setTitleText(activity.getResources().getString(R.string.info))
+                .setContentText(activity.getResources().getString(R.string.succes))
                 .show();
     }
 
@@ -65,10 +59,10 @@ public class DialogCreater {
                                       String question, int id) {
 
         new SweetAlertDialog(activity, SweetAlertDialog.WARNING_TYPE)
-                .setTitleText(SURE)
+                .setTitleText(activity.getResources().getString(R.string.sure))
                 .setContentText(question)
-                .setCancelText(NO)
-                .setConfirmText(YES)
+                .setCancelText(activity.getResources().getString(R.string.no))
+                .setConfirmText(activity.getResources().getString(R.string.yes))
                 .showCancelButton(true)
                 .setConfirmClickListener(sweetAlertDialog -> {
 
@@ -97,7 +91,7 @@ public class DialogCreater {
         return pDialog;
     }
 
-    public static SweetAlertDialog loadingDialog(Activity activity, String loadingText,boolean cancelable) {
+    public static SweetAlertDialog loadingDialog(Activity activity, String loadingText, boolean cancelable) {
 
         SweetAlertDialog pDialog = new SweetAlertDialog(activity, SweetAlertDialog.PROGRESS_TYPE);
         pDialog.getProgressHelper().setBarColor(Color.parseColor(LOADING_COLOR));
@@ -117,17 +111,17 @@ public class DialogCreater {
 
         SweetAlertDialog pDialog = new SweetAlertDialog(activity, SweetAlertDialog.PROGRESS_TYPE);
         pDialog.getProgressHelper().setBarColor(Color.parseColor(LOADING_COLOR));
-        pDialog.setTitleText(LOADING);
+        pDialog.setTitleText(activity.getResources().getString(R.string.loading));
         pDialog.setCancelable(false);
         pDialog.show();
         return pDialog;
     }
 
-    public static SweetAlertDialog loadingDialog(Activity activity,boolean cancelable) {
+    public static SweetAlertDialog loadingDialog(Activity activity, boolean cancelable) {
 
         SweetAlertDialog pDialog = new SweetAlertDialog(activity, SweetAlertDialog.PROGRESS_TYPE);
         pDialog.getProgressHelper().setBarColor(Color.parseColor(LOADING_COLOR));
-        pDialog.setTitleText(LOADING);
+        pDialog.setTitleText(activity.getResources().getString(R.string.loading));
         pDialog.setCancelable(cancelable);
         pDialog.showCancelButton(cancelable);
         pDialog.setCancelClickListener(sDialog -> {
@@ -140,30 +134,30 @@ public class DialogCreater {
 
     public static void doneDialog(Activity activity) {
         new SweetAlertDialog(activity, SweetAlertDialog.SUCCESS_TYPE)
-                .setTitleText(INFO)
-                .setContentText(SUCCES)
+                .setTitleText(activity.getResources().getString(R.string.info))
+                .setContentText(activity.getResources().getString(R.string.succes))
                 .setConfirmClickListener(sweetAlertDialog -> sweetAlertDialog.dismissWithAnimation())
                 .show();
     }
 
     public static void failDialog(Activity activity) {
         new SweetAlertDialog(activity, SweetAlertDialog.ERROR_TYPE)
-                .setTitleText(INFO)
-                .setContentText(FAIL)
+                .setTitleText(activity.getResources().getString(R.string.info))
+                .setContentText(activity.getResources().getString(R.string.fail))
                 .setConfirmClickListener(sweetAlertDialog -> sweetAlertDialog.dismissWithAnimation())
                 .show();
     }
 
     public static void changeDoneDialog(Activity activity, String question) {
         new SweetAlertDialog(activity, SweetAlertDialog.WARNING_TYPE)
-                .setTitleText(SURE)
+                .setTitleText(activity.getResources().getString(R.string.sure))
                 .setContentText(question)
-                .setConfirmText(YES)
-                .setCancelText(NO)
+                .setConfirmText(activity.getResources().getString(R.string.yes))
+                .setCancelText(activity.getResources().getString(R.string.no))
                 .setConfirmClickListener(sDialog -> sDialog
-                        .setTitleText(INFO)
-                        .setContentText(SUCCES)
-                        .setConfirmText(OK)
+                        .setTitleText(activity.getResources().getString(R.string.info))
+                        .setContentText(activity.getResources().getString(R.string.succes))
+                        .setConfirmText(activity.getResources().getString(R.string.ok))
                         .showCancelButton(false)
                         .setConfirmClickListener(null)
                         .changeAlertType(SweetAlertDialog.SUCCESS_TYPE))
@@ -172,14 +166,14 @@ public class DialogCreater {
 
     public static void changeFailDialog(Activity activity, String question) {
         new SweetAlertDialog(activity, SweetAlertDialog.WARNING_TYPE)
-                .setTitleText(SURE)
+                .setTitleText(activity.getResources().getString(R.string.sure))
                 .setContentText(question)
-                .setConfirmText(YES)
-                .setCancelText(NO)
+                .setConfirmText(activity.getResources().getString(R.string.yes))
+                .setCancelText(activity.getResources().getString(R.string.no))
                 .setCancelClickListener(sDialog -> sDialog
-                        .setTitleText(INFO)
-                        .setContentText(FAIL)
-                        .setConfirmText(OK)
+                        .setTitleText(activity.getResources().getString(R.string.info))
+                        .setContentText(activity.getResources().getString(R.string.fail))
+                        .setConfirmText(activity.getResources().getString(R.string.ok))
                         .showCancelButton(false)
                         .setConfirmClickListener(null)
                         .changeAlertType(SweetAlertDialog.ERROR_TYPE))
@@ -188,21 +182,21 @@ public class DialogCreater {
 
     public static void changeFailOrDoneDialog(Activity activity, String question) {
         new SweetAlertDialog(activity, SweetAlertDialog.WARNING_TYPE)
-                .setTitleText(SURE)
+                .setTitleText(activity.getResources().getString(R.string.sure))
                 .setContentText(question)
-                .setConfirmText(YES)
-                .setCancelText(NO)
+                .setConfirmText(activity.getResources().getString(R.string.yes))
+                .setCancelText(activity.getResources().getString(R.string.no))
                 .setCancelClickListener(sDialog -> sDialog
-                        .setTitleText(INFO)
-                        .setContentText(FAIL)
-                        .setConfirmText(OK)
+                        .setTitleText(activity.getResources().getString(R.string.info))
+                        .setContentText(activity.getResources().getString(R.string.fail))
+                        .setConfirmText(activity.getResources().getString(R.string.ok))
                         .showCancelButton(false)
                         .setConfirmClickListener(null)
                         .changeAlertType(SweetAlertDialog.ERROR_TYPE))
                 .setConfirmClickListener(sDialog -> sDialog
-                        .setTitleText(INFO)
-                        .setContentText(SUCCES)
-                        .setConfirmText(OK)
+                        .setTitleText(activity.getResources().getString(R.string.info))
+                        .setContentText(activity.getResources().getString(R.string.succes))
+                        .setConfirmText(activity.getResources().getString(R.string.ok))
                         .showCancelButton(false)
                         .setConfirmClickListener(null)
                         .changeAlertType(SweetAlertDialog.SUCCESS_TYPE))
